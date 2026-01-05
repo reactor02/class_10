9420cb9a7b2a53f62e7729780d4b15f75e5b493f
gnadbguy@gmail.com <>
2026-01-05 17:22:45 +0900

    java 올립니다

---
+++ workspace_java/chap00/src/chap00/Hello.java
@@ -0,0 +1,23 @@
+package chap00;
+
+public class Hello {
+
+	// 한 줄 주석
+	/*범위 주석*/
+	/* 여러 줄 
+	 * 주석
+	 */
+	/**
+	 * javadoc 주석
+	 * @param args
+	 */
+	/*
+	 * 주석과 출력이 가장 중요하다
+	 */
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World"); //출력문
+	}
+
+}
---
+++ workspace_java/chap02/src/chap02/VarTypeExam.java
@@ -0,0 +1,32 @@
+package chap02;
+
+public class VarTypeExam {
+
+	public static void main(String[] args) {
+		
+		byte b1;
+		b1 = 127;
+		System.out.println("b1: " + b1);
+//		b1 = 128;
+		
+		char c1 = 65;
+		System.out.println("c1: " + c1);
+		char c2 = 65 + 2;
+		System.out.println("c2: " + c2);
+		char c3 = 'B';
+		System.out.println("c3 - c1: " + (c3 - c1));
+		
+		//L또는 l을 붙이면 명시적인 long
+		long l1 = 22000000000L;
+		long l2 = 31;
+		
+		String s1 = "이 왕 재";
+		System.out.println("s1: " + s1);
+		
+		String s2 = "이 \"왕 재";
+		System.out.println("s2: " + s2);
+		String s3 = "\\이 \t왕 \n재";
+		System.out.println(s3);
+	}
+
+}
